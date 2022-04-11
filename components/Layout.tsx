0b7af7a1cd2f60fbe69bf9ab47d178d15345bc67
Jai Keery <jai.seth.keery@gmail.com>
2022-04-11 18:39:38 +0800

    Init

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,17 +2,20 @@ import { Container } from '@chakra-ui/react';
 import { Token } from '@chakra-ui/styled-system/dist/declarations/src/utils';
 import { OrderType } from '@prisma/client';
 import * as CSS from 'csstype';
+import { NextSeo } from 'next-seo';
 
 import Header from '../components/Header';
 
 interface Props {
+  title?: string;
   mode: OrderType;
   size: Token<CSS.Property.MaxWidth | number, "sizes">;
 }
 
-const Layout: React.FC<Props> = ({ children, size, mode }) => {
+const Layout: React.FC<Props> = ({ children, mode, size, title }) => {
   return (
     <>
+      <NextSeo title={title} />
       <Header mode={mode} />
       <Container as="main" p={0} maxW={size}>
         {children}
