2bbe0eccd13309262e30798a4ce6c2a3cfa3a187
Jai Keery <jai.seth.keery@gmail.com>
2022-04-14 19:29:30 +0800

    Init

---
+++ components/Order/OrderSummary.tsx
@@ -0,0 +1,123 @@
+import { Badge, Box, Button, Heading, Icon, SimpleGrid, Text, Tooltip } from '@chakra-ui/react';
+import dayjs from 'dayjs';
+import React from 'react';
+import { FaClock, FaEnvelope, FaShippingFast, FaStripeS } from 'react-icons/fa';
+import Stripe from 'stripe';
+
+import CustomerInfo from '../../components/Order/CustomerInfo';
+import ResendEmail from '../../components/Order/ResendEmail';
+import { FullOrder } from '../../types';
+import { FullCheckoutSession } from '../../types/stripe';
+import { formatAmountForDisplay } from '../../utils/stripeHelpers';
+
+interface Props {
+  order: FullOrder;
+  checkout?: FullCheckoutSession;
+  itemsList: React.ReactNode[];
+}
+
+const OrderSummary: React.FC<Props> = ({ order, checkout, itemsList }) => {
+  const customer = checkout?.customer as Stripe.Customer | undefined;
+  const shipping = checkout?.shipping as
+    | Stripe.Checkout.Session.Shipping
+    | undefined;
+
+  return (
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={8} mb={4}>
+      <Box>
+        <Heading size='md' fontFamily='body' p={2} mb={2} borderBottomWidth={1}>
+          Customer Details
+        </Heading>
+        <CustomerInfo user={customer} shipping={shipping} />
+      </Box>
+      <Box>
+        <Heading size='md' fontFamily='body' p={2} mb={2} borderBottomWidth={1}>
+          Order Details
+        </Heading>
+        <Box px={2}>
+          <Text>
+            <Icon as={FaClock} mr={2} />
+            {dayjs(order.createdAt).format('LLL')}
+          </Text>
+          <Badge fontFamily='mono' mr={1}>
+            ID: {order.id}
+          </Badge>
+          <Badge
+            colorScheme={
+              order.status === 'PAID'
+                ? 'green'
+                : order.status === 'PENDING'
+                ? 'orange'
+                : order.status === 'SHIPPED'
+                ? 'blue'
+                : 'red'
+            }
+            mr={1}
+          >
+            {`Payment Status: ${order?.status ?? ''}`}
+          </Badge>
+          {order?.hasSentOrderEmail && order.hasSentReceiptEmail && (
+            <Tooltip hasArrow label='Receipt Emailed'>
+              <Badge mr={1}>
+                <Icon as={FaEnvelope} />
+              </Badge>
+            </Tooltip>
+          )}
+          {order.status === 'SHIPPED' && (
+            <Tooltip hasArrow label='Order Shipped'>
+              <Badge mr={1}>
+                <Icon as={FaShippingFast} />
+              </Badge>
+            </Tooltip>
+          )}
+          {checkout?.payment_intent?.charges.data[0]?.receipt_url && (
+            <Box pt={2}>
+              <Button
+                as='a'
+                href={
+                  checkout?.payment_intent?.charges.data[0]?.receipt_url ?? ''
+                }
+                target='_blank'
+                size='sm'
+                colorScheme='purple'
+                leftIcon={<Icon as={FaStripeS} />}
+              >
+                Stripe Receipt
+              </Button>
+            </Box>
+          )}
+          {checkout?.url && (
+            <Box pt={2}>
+              <Button
+                as='a'
+                href={checkout.url}
+                target='_blank'
+                size='sm'
+                colorScheme='purple'
+                leftIcon={<Icon as={FaStripeS} />}
+              >
+                Continue Checkout
+              </Button>
+            </Box>
+          )}
+          {!order.hasSentReceiptEmail && checkout && (
+            <Box pt={2}>
+              <ResendEmail orderId={order.id} checkoutId={checkout.id} />
+            </Box>
+          )}
+          <Text fontWeight='bold' mt={2} py={1} borderBottomWidth={1}>
+            Items
+          </Text>
+          {itemsList}
+          <Box>
+            <Text fontSize='2xl' textAlign='right'>
+              {formatAmountForDisplay(checkout?.amount_total ?? 0)}
+            </Text>
+          </Box>
+        </Box>
+      </Box>
+    </SimpleGrid>
+  );
+};
+
+export default OrderSummary;
